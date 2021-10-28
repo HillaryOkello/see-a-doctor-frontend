0533f99a81412c8a88c74904e649277a90b0d35f
Hillary Okello <hillariouskelly@outlook.com>
2021-10-28 11:06:09 +0300

    Implement Register component and functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  Switch, Route, useLocation,
+} from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Register from './components/Register';
+import { clearMessage } from './actions/message';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  useEffect(() => {
+    dispatch(clearMessage());
+  }, [dispatch, location]);
+
   return (
     <div>
-      <h1>See a Doctor</h1>
+      <main>
+        <Switch>
+          <Route exact path={['/', '/home']} component={Home} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+import store from './store';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <AlertProvider template={AlertTemplate}>
+        <App />
+      </AlertProvider>
+    </Router>
+  </Provider>,
   document.getElementById('root'),
 );
